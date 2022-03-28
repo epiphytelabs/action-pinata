5a15e822d8b9bb9d75df4d41c53e36caea30e3db
David Dollar <david@dollar.io>
2022-03-28 18:47:18 -0400

    pin by cid

--- index.js
+++ index.js
@@ -3,29 +3,17 @@ const path = require("path");
 const pinata = require("@pinata/sdk")(core.getInput("key"), core.getInput("secret"));
 
 async function main() {
-	try {
-		let source = core.getInput("path");
+	const cid = core.getInput("cid");
 
-		if (!path.isAbsolute(source)) {
-			const root = (process.env.GITHUB_WORKSPACE || process.cwd()).toString();
-			source = path.join(root, source);
-		}
-
-		const options = {
-			cidVersion: 0,
-			wrapWithDirectory: false,
-		};
-
-		const result = await pinata.pinFromFS(source, options);
-		const cid = result.IpfsHash.toString();
-
-		core.setOutput("cid", cid);
-
-		console.log("Published", cid);
-	} catch (error) {
-		core.setFailed(error.message);
-		throw error;
-	}
+	pinata
+		.pinByHash(cid.toString())
+		.then((result) => {
+			core.setOutput("cid", result.ipfsHash);
+			console.log("Pinned", cid);
+		})
+		.catch((error) => {
+			core.setFailed(error.message);
+		});
 }
 
 main();
